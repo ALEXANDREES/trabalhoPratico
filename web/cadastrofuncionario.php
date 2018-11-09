6c2a3aaf2e770a685b126fc235bd09572b274b64
lucasgli <eslucasgabriel@gmail.com>
2018-11-09 19:41:57 -0300

    Resolvendo Conflito

--- web/cadastrofuncionario.php
+++ web/cadastrofuncionario.php
@@ -1,151 +1,192 @@
 <!DOCTYPE html>
+<html lang="pt-br">
 
-<html lang="pt br">
+  <head>
 
-<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <meta http-equiv=Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>Cadrastro Funcionario</title>
+    <title>LocaPlus</title>
 
-  <!-- CSS  -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  
-</head>
-<body>
-  <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">LocaPlus</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Clientes</a></li>
-        <li><a href="badges.html">Funcionario</a></li>
-<li><a href="badges.html">Locação</a></li>
-        <li><a href="collapsible.html">Veiculos</a></li>
-      </ul>
-    </div>
-	</nav>
-  <div class="container">
-  
-	<div class="section">
-	
-		<div class="row">
-		
-      <form id="Formulario" action="action.php">
-	  
-  			<div class="col s6 offset-s3">
-			
-          <div class="row">
-		  
-            <div class="input-field col s12">
-			
-              <input id="nome" name="nome" type="text" class="form validate">
-			  
-              <label for="nome">Digite seu Nome</label>
-			  
-            </div>          
-			
-          </div>
-		   <div class="row">
-		  
-            <div class="input-field col s12">
-			
-              <input id="rg" name="rg" type="text" class="form validate">
-			  
-              <label for="rg">Informe seu RG</label>
-			  
-            </div>          
-			
-          </div>
-		   <div class="row">
-		  
-            <div class="input-field col s12">
-			
-              <input id="cpf" name="cpf" type="text" class="form validate">
-			  
-              <label for="cpf">Digite seu CPF</label>
-			  
-            </div>          
-			
-          </div>
-		  
-		  <div class="row">
-		  
-            <div class="input-field col s12">
-			
-              <input id="end" name="end" type="text" class="form validate">
-			  
-              <label for="end">Digite seu endereço </label>
-			  
-            </div>          
-			
-          </div>
-		  
-		  <div class="row">
-		  
-            <div class="input-field col s12">
-			
-              <input id="cnh" name="cnh" type="text" class="form validate">
-			  
-              <label for="cnh">Digite seu Cargo </label>
-			  
-            </div>          
-			
-          </div>
-		   <div class="row">
-            <div class="col s12">
-             <input id="dataNasc" name="dataAdmissão" type="text" class="datepicker form">
-           </div>
-          </div>
-		  
-		  
-		  
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/estilos.css" rel="stylesheet">
 
-          <div class="row">
-            <button class="waves-effect waves-light btn blue-grey darken-3 z-depth-5" type="cadastrar" name="action">Cadastrar
-          
-            </button>
-			<button class="waves-effect waves-light btn blue-grey darken-3 z-depth-5" type="editar" name="action">Editar
-            
-            </button>
-			<button class="waves-effect waves-light btn blue-grey darken-3 z-depth-5" type="excluir" name="action">Excluir
-       
-            </button>
-          </div>
-		  
+    <!-- Custom styles for this template -->
+    <link href="css/half-slider.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+	  <img src='imagens/icon.png'>
+        <a class="navbar-brand" href="#">LocaPlus</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Inicio
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Veiculos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Locação</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Login</a>
+            </li>
+          </ul>
         </div>
-      </form>
-		</div>
-	</div>
-  </div>
+      </div>
+    </nav>
 
-  <!-- Footer  -->
-          <footer class="page-footer">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Sobre a empresa</h5>
-                <p class="grey-text text-lighten-4">
-Informações ao consumidor: LocaPlus <br>
-E-mail: Locaplus.br
-</p>
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright">
-            <div class="container">
-            2018 Copyright
-            <a class="grey-text text-lighten-4 right" href="#!">Mais Informações</a>
-            </div>
-          </div>
-        </footer>
-		
-		
-		
-  <!--  Scripts-->
-  <script src="js/materialize.min.js"></script>
-  <script src="js/materialize.js"></script>
+
+
+
+   
+   
+  
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    
+<div class="row" id ="divta">
+    <div class="col"></div>
+ <div class="col"> <table id="tabelav" class="table table-bordered table-striped table-responsive" >
+
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Placa</th>
+      <th>Nome</th>
+      <th>Marca</th>
+      <th>Modelo</th>
+      <th>Valor Seguro</th>
+      <th>Valor Locação</th>
+      <th>Cor</th>
+      <th>Ativo</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+   
+    </tr>
+    
+    
+  </tbody>
+
+</table>
+</div>
+    
+  <div class="col">   
+              
+
+    </div>
+    
+</div>
+    
+    <div class="row">
+  <div class="col-sm-4"></div>
+  <div class="col-sm-8"><button type="button" id ="modalb" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+    
+  Cadastrar
+</button>
+     <button type="button"  id ="modalb" class="btn btn-primary">Remover</button> 
+     <button type="button"  id ="modalb" class="btn btn-primary">Editar</button>
+</div>
+  
+</div>
+    
+  <div class="footer">
+  <p>Footer</p>
+</div>
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Cadastro Veiculo</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+                        <form>
+                    <div class="form-group">
+                        <label >Placa</label>
+                        <input  class="form-control" id="exampleInputEmail1"  placeholder=>
+                        <small id="emailHelp" class="form-text text-muted"></small>
+                    </div>
+                    <div class="form-group">
+                        <label >Nome</label>
+                        <input  class="form-control" id="exampleInputPassword1" >
+                    </div>
+                    <div class="form-group">
+                        <label >Marca</label>
+                        <input class="form-control" id="exampleInputPassword1" >
+                    </div>
+                    <div class="form-group">
+                        <label >Modelo</label>
+                        <input class="form-control" id="exampleInputPassword1" >
+                    </div>
+                    <div class="form-group">
+                        <label >Valor Seguro</label>
+                        <input class="form-control" id="exampleInputPassword1" >
+                    </div>
+                     <div class="form-group">
+                        <label >Valor Locaçao</label>
+                        <input class="form-control" id="exampleInputPassword1" >
+                    </div>
+                       <div class="form-group">
+                        <label >Cor</label>
+                        <input class="form-control" id="exampleInputPassword1" >
+                    </div>
+                      <div class="form-group">
+                        <label for="exampleFormControlSelect1">Ativo</label>
+                        <select class="form-control" id="exampleFormControlSelect1">
+                            <option>Sim</option>
+                            <option>Não</option>
+
+                        </select>
+                    </div>
+
+                </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Sair</button>
+        <button type="button" class="btn btn-primary">Salvar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   </body>
+
 </html>
